486f1d262475b3b851b443a1f6d1ad2369820e0b
Lucas Gabriel <adornilucas74@gmail.com>
2026-03-10 12:02:30 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,9 @@
+// ======================================
+//   NOSSA API DE CACHORROS
+//   Feita com Node.js + Express
+
+//  Agora as fotos NÃO são mais baixadas automaticamnetes.
+//  Elas devem existir manualmente na pasta:
+
+//  data/fotos/
+// ====================================
