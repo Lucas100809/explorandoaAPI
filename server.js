d2f70b8009d78b95db628db1a86dee41983b5660
Lucas Gabriel <adornilucas74@gmail.com>
2026-03-16 12:06:14 -0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-// ======================================
-//   NOSSA API DE CACHORROS
-//   Feita com Node.js + Express
-
-//  Agora as fotos NÃO são mais baixadas automaticamnetes.
-//  Elas devem existir manualmente na pasta:
-
-//  data/fotos/
-// ====================================
